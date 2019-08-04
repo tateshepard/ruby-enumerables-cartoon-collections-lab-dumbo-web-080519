2928b96e26790a39a804cd8e572f7aae00e1f37f
Tate Shepard <tateshepard@yahoo.com>
2019-08-04 21:49:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
 end
+
 
 def summon_captain_planet# code an argument here
   # Your code here
